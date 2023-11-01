1d7874c782ae1d9b4e3d908f2611cccd5d037a3a
Davide Cazzin <28535750+NeverMendel@users.noreply.github.com>
2023-11-01 19:07:25 +0100

    Add tests to heif-convert

---
+++ tests/test-script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+heif-convert image.heic -f jpg -q 90 -o result
+heif-convert image.heic -f png -o result
+
+status_code=0
+
+if ! cmp -s image.jpg result.jpg; then
+  echo "jpg image is different"
+  status_code=1
+fi
+
+if ! cmp -s image.png result.png; then
+  echo "png image is different"
+  status_code=1
+fi
+
+exit $status_code
